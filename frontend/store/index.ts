07ce00a735ed56eccfb57c235306262ac0d9ec8c
intellieseawolf <intelliseawolf103@gmail.com>
2022-04-18 11:32:01 -0400

    added: redux store index

---
+++ frontend/store/index.ts
@@ -0,0 +1,48 @@
+import {
+  Action,
+  AnyAction,
+  combineReducers,
+  configureStore,
+  ThunkAction,
+} from "@reduxjs/toolkit";
+import { createWrapper, HYDRATE } from "next-redux-wrapper";
+
+import authReducer from "./modules/auth.slice";
+
+const combinedReducer = combineReducers({
+  auth: authReducer,
+});
+
+const reducer = (
+  state: ReturnType<typeof combinedReducer>,
+  action: AnyAction
+) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state, // use previous state
+      ...action.payload, // apply delta from hydration
+    };
+    return nextState;
+  } else {
+    return combinedReducer(state, action);
+  }
+};
+
+export const makeStore = () =>
+  configureStore({
+    // @ts-ignore
+    reducer,
+  });
+
+export const wrapper = createWrapper(makeStore, { debug: true });
+
+type Store = ReturnType<typeof makeStore>;
+
+export type AppDispatch = Store["dispatch"];
+export type RootState = ReturnType<Store["getState"]>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
